11b2fdf34b73fe9fe75da41e8fcb068049605ab2
DevDyna <andrearisorti2003@gmail.com>
2025-09-15 23:41:00 +0200

    Create Main.java

---
+++ src/main/java/com/devdyna/mekagenjei/Main.java
@@ -0,0 +1,15 @@
+package com.devdyna.mekagenjei;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+@Mod(Main.ID)
+public class Main {
+
+    public static final String ID = "mekagenjei";
+
+    public Main(IEventBus bus, ModContainer mc) {
+
+    }
+}
