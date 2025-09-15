3c1f6daa85b113cccbc17d94c73380b088cb33b6
DevDyna <andrearisorti2003@gmail.com>
2025-09-15 23:44:23 +0200

    Update Main.java

--- src/main/java/com/devdyna/mekagenjei/Main.java
+++ src/main/java/com/devdyna/mekagenjei/Main.java
@@ -9,6 +9,9 @@ public class Main {
 
     public static final String ID = "mekagenjei";
 
+    public static final String GAS_BURNING_GENERATOR_CATEGORY_ID = ID+".jei.gas_burning";
+    public static final String BIOFUEL_GENERATOR_CATEGORY_ID = ID+".jei.bio_fuel";
+
     public Main(IEventBus bus, ModContainer mc) {
 
     }
