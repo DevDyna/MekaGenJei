9a1b74c5bdcac28c5f82179d5042bd9a0ed9cf69
DevDyna <andrearisorti2003@gmail.com>
2025-11-15 15:16:41 +0100

    utils

--- src/main/java/com/devdyna/mekagenjei/utils/Numbers.java
+++ src/main/java/com/devdyna/mekagenjei/utils/Numbers.java
@@ -1,23 +1,44 @@
 package com.devdyna.mekagenjei.utils;
 
-import java.text.DecimalFormat;
-
 public class Numbers {
-    private static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("#.#");
-
+    
     public static String toCompact(double value) {
-
-        if (value < 1)
+        if (value < 1) {
             return String.valueOf(value);
+        }
 
-        if (value >= 1_000_000_000)
-            return DECIMAL_FORMAT.format(value / 1_000_000_000) + "B";
-        else if (value >= 1_000_000)
-            return DECIMAL_FORMAT.format(value / 1_000_000) + "M";
-        else if (value >= 1_000)
-            return DECIMAL_FORMAT.format(value / 1_000) + "k";
-        else
-            return DECIMAL_FORMAT.format(value);
+        String suffix = "";
+        double scaledValue = value;
 
+        if (value >= 1_000_000_000) {
+            scaledValue = value / 1_000_000_000;
+            suffix = "B";
+        } else if (value >= 1_000_000) {
+            scaledValue = value / 1_000_000;
+            suffix = "M";
+        } else if (value >= 1_000) {
+            scaledValue = value / 1_000;
+            suffix = "k";
+        }
+
+        double truncated = Math.floor(scaledValue * 10) / 10;
+
+        if (truncated == (long) truncated) {
+            return String.format("%d%s", (long) truncated, suffix);
+        } else {
+            return String.format("%.1f%s", truncated, suffix);
+        }
+    }
+
+    public static int jouleToFE(long j) {
+        return (int) (j * 2) / 5;
+    }
+
+    public static long feToJoule(int fe) {
+        return (long) (fe * 5) / 2;
+    }
+
+    public static String simplify(double value){
+        return value % 1 == 0 ? String.format("%.0f", value) : String.valueOf(value);
     }
 }
