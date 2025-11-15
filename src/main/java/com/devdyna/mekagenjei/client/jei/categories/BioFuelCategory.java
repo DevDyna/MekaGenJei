d4adc487d10199ea5bda9f11b112d1c4961e2f38
DevDyna <andrearisorti2003@gmail.com>
2025-11-15 02:09:06 +0100

    Update BioFuelCategory.java

--- src/main/java/com/devdyna/mekagenjei/client/jei/categories/BioFuelCategory.java
+++ src/main/java/com/devdyna/mekagenjei/client/jei/categories/BioFuelCategory.java
@@ -27,65 +27,68 @@ import mezz.jei.api.recipe.category.AbstractRecipeCategory;
 @SuppressWarnings({ "null" })
 public class BioFuelCategory<T> extends AbstractRecipeCategory<zStatic.BIOFUEL> {
 
-    private IGuiHelper guiHelper;
+        private IGuiHelper guiHelper;
 
-    public final static RecipeType<zStatic.BIOFUEL> TYPE = RecipeType.create(ID,
-            zStatic.categories.BIOFUEL_FUELS.key(),
-            zStatic.BIOFUEL.class);
+        public final static RecipeType<zStatic.BIOFUEL> TYPE = RecipeType.create(ID,
+                        zStatic.categories.BIOFUEL_FUELS.key(),
+                        zStatic.BIOFUEL.class);
 
-    public BioFuelCategory(IGuiHelper guiHelper) {
-        super(TYPE, Component.translatable(zStatic.categories.BIOFUEL_FUELS.key()),
-                ItemIcon.of(guiHelper, GeneratorsBlocks.BIO_GENERATOR.get().asItem()), 96, 32);
-        this.guiHelper = guiHelper;
-    }
+        public BioFuelCategory(IGuiHelper guiHelper) {
+                super(TYPE, Component.translatable(zStatic.categories.BIOFUEL_FUELS.key()),
+                                ItemIcon.of(guiHelper, GeneratorsBlocks.BIO_GENERATOR.get().asItem()), 96, 32);
+                this.guiHelper = guiHelper;
+        }
 
-    @Override
-    public void setRecipe(IRecipeLayoutBuilder builder, zStatic.BIOFUEL recipe, IFocusGroup group) {
-        builder.addInputSlot(1, 8).addItemStack(new ItemStack(recipe.getItems()));
-    }
+        @Override
+        public void setRecipe(IRecipeLayoutBuilder builder, zStatic.BIOFUEL recipe, IFocusGroup group) {
+                builder.addInputSlot(1, 8).addItemStack(new ItemStack(recipe.getItems()));
+        }
 
-    @Override
-    public void draw(zStatic.BIOFUEL recipe, IRecipeSlotsView recipeSlotsView, GuiGraphics guiGraphics, double mouseX,
-            double mouseY) {
-        var font = Minecraft.getInstance().font;
+        @Override
+        public void draw(zStatic.BIOFUEL recipe, IRecipeSlotsView recipeSlotsView, GuiGraphics guiGraphics,
+                        double mouseX,
+                        double mouseY) {
+                var font = Minecraft.getInstance().font;
 
-        guiHelper.drawableBuilder(x.rl("textures/gui/fuel_icons.png"), 0, 0, 96, 32).setTextureSize(96, 32).build()
-                .draw(guiGraphics, 0, 0);
+                guiHelper.drawableBuilder(x.rl("textures/gui/fuel_icons.png"), 0, 0, 96, 32).setTextureSize(96, 32)
+                                .build()
+                                .draw(guiGraphics, 0, 0);
 
-        var time = 200 * (x.item(recipe.getItems()).is(MekanismItems.BIO_FUEL) ? 1 : 9);
-        var rate = 280;
-        var total = time * rate;
+                var time = 200 * (x.item(recipe.getItems()).is(MekanismItems.BIO_FUEL) ? 1 : 9);
+                var rate = 280;
+                var total = time * rate;
 
-        PoseStack stack = guiGraphics.pose();
-        stack.pushPose();
-        stack.scale(0.75F, 0.75F, 8000F);
-        guiGraphics.drawString(font,
-                (Screen.hasShiftDown()
-                        ? time / 20 + " sec"
-                        : time + " ticks"),
-                46, 4,
-                0xFFFFFF);
-        guiGraphics.drawString(font, (Screen.hasShiftDown() ? Numbers.toCompact(rate) : rate) + " FE/tick", 46, 18,
-                0xFFFFFF);
-        guiGraphics.drawString(font, (Screen.hasShiftDown() ? Numbers.toCompact(total) : total) + " FE", 46, 32,
-                0xFFFFFF);
-        stack.popPose();
+                PoseStack stack = guiGraphics.pose();
+                stack.pushPose();
+                stack.scale(0.75F, 0.75F, 8000F);
+                guiGraphics.drawString(font,
+                                (Screen.hasShiftDown()
+                                                ? ((double) time / 20 +"").replaceAll("\\.0$", "") + " sec"
+                                                : time + " ticks"),
+                                46, 4,
+                                0xFFFFFF);
+                guiGraphics.drawString(font, (Screen.hasShiftDown() ? Numbers.toCompact(rate) : rate) + " FE/tick", 46,
+                                18,
+                                0xFFFFFF);
+                guiGraphics.drawString(font, (Screen.hasShiftDown() ? Numbers.toCompact(total) : total) + " FE", 46, 32,
+                                0xFFFFFF);
+                stack.popPose();
 
-    }
+        }
 
-    @Override
-    public void getTooltip(ITooltipBuilder tooltip, zStatic.BIOFUEL recipe, IRecipeSlotsView recipeSlotsView,
-            double mouseX, double mouseY) {
+        @Override
+        public void getTooltip(ITooltipBuilder tooltip, zStatic.BIOFUEL recipe, IRecipeSlotsView recipeSlotsView,
+                        double mouseX, double mouseY) {
 
-        if (Pos.of(21, 0).setSize(10, 10).test(mouseX, mouseY))
-            tooltip.add(Component.translatable(ID + ".jei.time"));
+                if (Pos.of(21, 0).setSize(10, 10).test(mouseX, mouseY))
+                        tooltip.add(Component.translatable(ID + ".jei.time"));
 
-        if (Pos.of(21, 11).setSize(10, 10).test(mouseX, mouseY))
-            tooltip.add(Component.translatable(ID + ".jei.rate"));
+                if (Pos.of(21, 11).setSize(10, 10).test(mouseX, mouseY))
+                        tooltip.add(Component.translatable(ID + ".jei.rate"));
 
-        if (Pos.of(21, 22).setSize(10, 10).test(mouseX, mouseY))
-            tooltip.add(Component.translatable(ID + ".jei.total"));
+                if (Pos.of(21, 22).setSize(10, 10).test(mouseX, mouseY))
+                        tooltip.add(Component.translatable(ID + ".jei.total"));
 
-    }
+        }
 
 }
