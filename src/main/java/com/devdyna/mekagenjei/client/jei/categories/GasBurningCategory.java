80858aafa1ee2e8a8394555a18372ddc5365b4f5
DevDyna <andrearisorti2003@gmail.com>
2025-11-15 15:16:58 +0100

    fixed broken values

--- src/main/java/com/devdyna/mekagenjei/client/jei/categories/GasBurningCategory.java
+++ src/main/java/com/devdyna/mekagenjei/client/jei/categories/GasBurningCategory.java
@@ -30,72 +30,82 @@ import net.minecraft.network.chat.Component;
 @SuppressWarnings("null")
 public class GasBurningCategory<T> extends AbstractRecipeCategory<zStatic.GASBURNING> {
 
-    protected IGuiHelper guiHelper;
+        protected IGuiHelper guiHelper;
 
-    public GasBurningCategory(IGuiHelper guiHelper) {
-        super(TYPE, Component.translatable(zStatic.categories.GAS_BURNING_FUELGAS.key()),
-                ItemIcon.of(guiHelper, GeneratorsBlocks.GAS_BURNING_GENERATOR.get().asItem()), 96, 32);
-        this.guiHelper = guiHelper;
-    }
+        public GasBurningCategory(IGuiHelper guiHelper) {
+                super(TYPE, Component.translatable(zStatic.categories.GAS_BURNING_FUELGAS.key()),
+                                ItemIcon.of(guiHelper, GeneratorsBlocks.GAS_BURNING_GENERATOR.get().asItem()), 96, 32);
+                this.guiHelper = guiHelper;
+        }
 
-    public final static RecipeType<zStatic.GASBURNING> TYPE = RecipeType.create(ID,
-            zStatic.categories.GAS_BURNING_FUELGAS.key(),
-            zStatic.GASBURNING.class);
+        public final static RecipeType<zStatic.GASBURNING> TYPE = RecipeType.create(ID,
+                        zStatic.categories.GAS_BURNING_FUELGAS.key(),
+                        zStatic.GASBURNING.class);
 
-    @Override
-    public void setRecipe(IRecipeLayoutBuilder builder, zStatic.GASBURNING recipe, IFocusGroup group) {
-        if (recipe instanceof IGasCategory cat)
-            builder.addInputSlot(1, 8)
-                    .addIngredient(MekanismJEI.TYPE_CHEMICAL,
-                            new ChemicalStack(cat.getGas(), 1000))
-                    .setCustomRenderer(MekanismJEI.TYPE_CHEMICAL, new ChemicalStackRenderer(1000, 16, 16));
-    }
+        @Override
+        public void setRecipe(IRecipeLayoutBuilder builder, zStatic.GASBURNING recipe, IFocusGroup group) {
+                if (recipe instanceof IGasCategory cat)
+                        builder.addInputSlot(1, 8)
+                                        .addIngredient(MekanismJEI.TYPE_CHEMICAL,
+                                                        new ChemicalStack(cat.getGas(), 1000))
+                                        .setCustomRenderer(MekanismJEI.TYPE_CHEMICAL,
+                                                        new ChemicalStackRenderer(1000, 16, 16));
+        }
 
-    @Override
-    public void draw(zStatic.GASBURNING recipe, IRecipeSlotsView recipeSlotsView, GuiGraphics guiGraphics,
-            double mouseX,
-            double mouseY) {
-        var font = Minecraft.getInstance().font;
+        @Override
+        public void draw(zStatic.GASBURNING recipe, IRecipeSlotsView recipeSlotsView, GuiGraphics guiGraphics,
+                        double mouseX,
+                        double mouseY) {
+                var font = Minecraft.getInstance().font;
 
-        guiHelper.drawableBuilder(x.rl("textures/gui/fuel_icons.png"), 0, 0, 96, 32).setTextureSize(96, 32).build()
-                .draw(guiGraphics, 0, 0);
+                guiHelper.drawableBuilder(x.rl("textures/gui/fuel_icons.png"), 0, 0, 96, 32).setTextureSize(96, 32)
+                                .build()
+                                .draw(guiGraphics, 0, 0);
 
-        var data = recipe.getGas().getData(IMekanismDataMapTypes.INSTANCE.chemicalFuel());
-        var check = data == null;
-        var time = check ? 0 : data.burnTicks();
-        var rate = check ? 0 : data.energyPerTick();
-        var total = time * rate;
+                var fuel = recipe.getGas().getData(IMekanismDataMapTypes.INSTANCE.chemicalFuel());
+                var check = fuel == null;
 
-        PoseStack stack = guiGraphics.pose();
-        stack.pushPose();
-        stack.scale(0.75F, 0.75F, 8000F);
-        guiGraphics.drawString(font,
-                (Screen.hasShiftDown()
-                        ? ((double) time / 20 +"").replaceAll("\\.0$", "") + " sec"
-                        : time + " ticks"),
-                46, 4,
-                0xFFFFFF);
-        guiGraphics.drawString(font, (Screen.hasShiftDown() ? Numbers.toCompact(rate) : rate) + " FE/tick", 46, 18,
-                0xFFFFFF);
-        guiGraphics.drawString(font, (Screen.hasShiftDown() ? Numbers.toCompact(total) : total) + " FE", 46, 32,
-                0xFFFFFF);
-        stack.popPose();
+                var time = check ? 0 : fuel.burnTicks();
+                var rate = check ? 0 : Numbers.jouleToFE(fuel.energyDensity()) * (double) 256 / fuel.burnTicks();
 
-    }
+                var total = time * rate;
 
-    @Override
-    public void getTooltip(ITooltipBuilder tooltip, zStatic.GASBURNING recipe, IRecipeSlotsView recipeSlotsView,
-            double mouseX, double mouseY) {
+                PoseStack stack = guiGraphics.pose();
+                stack.pushPose();
+                stack.scale(0.75F, 0.75F, 8000F);
+                guiGraphics.drawString(font,
+                                (Screen.hasShiftDown()
+                                                ? ((double) time / 20 + "").replaceAll("\\.0$", "") + " sec"
+                                                : time + " ticks"),
+                                46, 4,
+                                0xFFFFFF);
+                guiGraphics.drawString(font,
+                                (Screen.hasShiftDown() ? Numbers.toCompact(rate) : Numbers.simplify(rate)) + " FE/tick",
+                                46,
+                                18,
+                                0xFFFFFF);
+                guiGraphics.drawString(font,
+                                (Screen.hasShiftDown() ? Numbers.toCompact(total) : Numbers.simplify(total))
+                                                + " FE",
+                                46, 32,
+                                0xFFFFFF);
+                stack.popPose();
 
-        if (Pos.of(21, 0).setSize(10, 10).test(mouseX, mouseY))
-            tooltip.add(Component.translatable(ID + ".jei.time"));
+        }
 
-        if (Pos.of(21, 11).setSize(10, 10).test(mouseX, mouseY))
-            tooltip.add(Component.translatable(ID + ".jei.rate"));
+        @Override
+        public void getTooltip(ITooltipBuilder tooltip, zStatic.GASBURNING recipe, IRecipeSlotsView recipeSlotsView,
+                        double mouseX, double mouseY) {
 
-        if (Pos.of(21, 22).setSize(10, 10).test(mouseX, mouseY))
-            tooltip.add(Component.translatable(ID + ".jei.total"));
+                if (Pos.of(21, 0).setSize(10, 10).test(mouseX, mouseY))
+                        tooltip.add(Component.translatable(ID + ".jei.time"));
 
-    }
+                if (Pos.of(21, 11).setSize(10, 10).test(mouseX, mouseY))
+                        tooltip.add(Component.translatable(ID + ".jei.rate"));
+
+                if (Pos.of(21, 22).setSize(10, 10).test(mouseX, mouseY))
+                        tooltip.add(Component.translatable(ID + ".jei.total"));
+
+        }
 
 }
