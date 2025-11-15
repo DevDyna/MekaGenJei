6d0a5895f911caf19f316cc34532dad195cad51d
DevDyna <andrearisorti2003@gmail.com>
2025-11-15 02:09:03 +0100

    Update Numbers.java

--- src/main/java/com/devdyna/mekagenjei/utils/Numbers.java
+++ src/main/java/com/devdyna/mekagenjei/utils/Numbers.java
@@ -3,19 +3,21 @@ package com.devdyna.mekagenjei.utils;
 import java.text.DecimalFormat;
 
 public class Numbers {
-     private static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("#.#");
+    private static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("#.#");
 
-    public static String toCompact(long value) {
-        double v = value;
+    public static String toCompact(double value) {
 
-        if (value >= 1_000_000_000) {
-            return DECIMAL_FORMAT.format(v / 1_000_000_000) + "B";
-        } else if (value >= 1_000_000) {
-            return DECIMAL_FORMAT.format(v / 1_000_000) + "M";
-        } else if (value >= 1_000) {
-            return DECIMAL_FORMAT.format(v / 1_000) + "k";
-        } else {
-            return Long.toString(value);
-        }
+        if (value < 1)
+            return String.valueOf(value);
+
+        if (value >= 1_000_000_000)
+            return DECIMAL_FORMAT.format(value / 1_000_000_000) + "B";
+        else if (value >= 1_000_000)
+            return DECIMAL_FORMAT.format(value / 1_000_000) + "M";
+        else if (value >= 1_000)
+            return DECIMAL_FORMAT.format(value / 1_000) + "k";
+        else
+            return DECIMAL_FORMAT.format(value);
+
     }
 }
